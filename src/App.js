983444253e8bd8294d9f0f63800b36c1665b678d
jessekim <jessekim.ck.94@gmail.com>
2019-09-25 10:58:28 +0900

    React Helmet Configurations

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {Helmet} from "react-helmet";
 import Nav from "./components/Nav";
 import { connect } from 'react-redux'
 import {getCurrentUser, refreshToken} from "./apis/apis"
 import { login } from "./redux/actions"
 import styles from './app.module.css'
+import favicon from './assets/favicon.ico'
 
 import Index from './routes/index'
 import Login from './routes/login'
@@ -31,6 +33,13 @@ class App extends React.Component {
   render() {
       return (
           <Router>
+              <div className="application">
+                  <Helmet>
+                      <meta charset="utf-8"/>
+                      <title>Jessekim's Blog</title>
+                      <link rel="shortcut icon" href={favicon} />
+                  </Helmet>
+              </div>
               <div>
                   <div>
                       <Nav />
